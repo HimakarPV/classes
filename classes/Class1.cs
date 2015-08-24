2fb31baba30d9a468b9877ad3bcc5b5f65bfda85
Himakar <himakar2009@gmail.com>
2015-08-24 11:16:33 +0530

    Add App

---
+++ classes/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes
+{
+    public class Class1
+    {
+
+        public static void details()
+        {
+            employee e = new employee();
+            e.name = "Himakar";
+            e.dob = "09-10-1993";
+            e.city = "Bangalore";
+            string EmployeeCode = "AZ016";
+
+            Console.WriteLine("Enter your employee code : ");
+            string x = Console.ReadLine().ToUpper();
+            if (EmployeeCode == x)
+            {
+                Console.WriteLine("\nEMPLOYEE DETAILS \n");
+                Console.WriteLine("NAME : {0} \nDOB : {1} \nLOCATON : {2} ",
+                                    e.name, e.dob, e.city);
+
+
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("You entered the wrong code..");
+                Console.ReadLine();
+            }
+
+        }
+
+        public static void getCompanyName(string name)
+        {
+            if(name.ToUpper()== "AEZION")
+
+            {
+                Class1.details();
+            }
+            else
+            {
+                Console.WriteLine("Enter correct company name..");
+                Console.ReadLine();
+            }
+        }
+        
+    }
+    public class employee
+    {
+        public string name { get; set; }
+        public string dob { get; set; }
+        public string city { get; set; }
+
+    }
+   
+}
---
+++ classes/Program.cs
@@ -0,0 +1,18 @@
+﻿using classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Show the Company details of :");
+            Class1.getCompanyName( Console.ReadLine());       
+        }
+    }
+}
